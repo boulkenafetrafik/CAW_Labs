72f2d3541ed081460768a6e1a905c5339c865d5d
LokManX <lokmanchekraoui@gmail.com>
2024-12-16 19:57:35 +0100

    some changes

--- Lab7/resturant-menu/src/App.js
+++ Lab7/resturant-menu/src/App.js
@@ -3,9 +3,11 @@ import './App.css';
 import React from 'react';
 import MenuDisplay from './components/MenuDisplay';
 import Cart from './components/Cart';
+import Navbar from './components/Navbar';
 function App() {
   return (
     <div className="App">
+      <Navbar/>
       <MenuDisplay />
     </div>
   );
--- Lab7/resturant-menu/src/components/Cart.jsx
+++ Lab7/resturant-menu/src/components/Cart.jsx
@@ -3,6 +3,7 @@
 import React, { useEffect, useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import './styles/cart.css';
+import Navbar from './Navbar';
 
 function Cart() {
     const [items, setItems] = useState([]);
@@ -45,36 +46,40 @@ function Cart() {
     const totalPrice = items.reduce((total, item) => total + item.price * item.quantity, 0);
 
     return (
-        <div className='cart' >
-            <h1 >Cart</h1>
-            <ul >
-                {items.map(item => (
-                    <li key={item.id} >
-                        <img src={item.image} alt={item.name}  />
-                        <span style={{ flex: 1 }}>{item.name} - {item.price} DA</span>
-                        <div className="btns" >
-                            <button 
-                                onClick={() => decreaseQuantity(item.id)} 
-                            >
-                                -
-                            </button>
-                            <span>{item.quantity}</span>
-                            <button 
-                                onClick={() => increaseQuantity(item.id)} 
-                                style={{ padding: '5px 10px', cursor: 'pointer' }}
-                            >
-                                +
-                            </button>
-                        </div>
-                        
-                    </li>
-                ))}
-            </ul>
-            <h2 >Total Price: {totalPrice} DA</h2>
-            <div className="checkout">
-                <Link to = "/order" state = {items}><button style={{ padding: '10px 20px', marginTop: '10px', cursor: 'pointer' }}>Place Order</button></Link>
+        <div className='' >
+            <Navbar/>
+            <div className="cart">
+                <h1 >Cart</h1>
+                <ul >
+                    {items.length == 0 ? <h2>your cart is empty</h2> :  items.map(item => (
+                        <li key={item.id} >
+                            <img src={item.image} alt={item.name}  />
+                            <span style={{ flex: 1 }}>{item.name} - {item.price} DA</span>
+                            <div className="btns" >
+                                <button 
+                                    onClick={() => decreaseQuantity(item.id)} 
+                                >
+                                    -
+                                </button>
+                                <span>{item.quantity}</span>
+                                <button 
+                                    onClick={() => increaseQuantity(item.id)} 
+                                    style={{ padding: '5px 10px', cursor: 'pointer' }}
+                                >
+                                    +
+                                </button>
+                            </div>
+                            
+                        </li>
+                    ))}
+                </ul>
+                <h2 >Total Price: {totalPrice} DA</h2>
+                <div className="checkout" style={{opacity: items.length == 0 ? 0.5 : 1, pointerEvents: items.length == 0 ? 'none' : 'all'}}>
+                    <Link to = "/order" state = {items}><button style={{ padding: '10px 20px', marginTop: '10px', cursor: 'pointer' }}>Place Order</button></Link>
 
+                </div>
             </div>
+           
         </div>
     );
 }
